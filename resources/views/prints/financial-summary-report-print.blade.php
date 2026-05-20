09ceb8fa2b34afc51aa04386e6916b0e7a558c02
mo7ashraf <mohammed.ashraf2011@gmail.com>
2026-05-21 02:45:10 +0300

    Refactor financial summary print view styles

--- resources/views/prints/financial-summary-report-print.blade.php
+++ resources/views/prints/financial-summary-report-print.blade.php
@@ -6,7 +6,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    <x-erp.print-page-styles orientation="portrait" />
+    <x-erp.print-page-styles orientation="landscape" />
 </head>
 <body>
 
