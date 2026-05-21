65336562dffcb7fc87846f035655924211c42a66
mo7ashraf <mohammed.ashraf2011@gmail.com>
2026-05-21 03:00:01 +0300

    Self-host Cairo font for offline ERP printing

--- resources/views/components/erp/print-page-styles.blade.php
+++ resources/views/components/erp/print-page-styles.blade.php
@@ -3,6 +3,53 @@
 ])
 
 <style>
+    @font-face {
+        font-family: "Cairo";
+        src: url("{{ asset('assets/fonts/cairo/Cairo-Regular.ttf') }}") format("truetype");
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: "Cairo";
+        src: url("{{ asset('assets/fonts/cairo/Cairo-Medium.ttf') }}") format("truetype");
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: "Cairo";
+        src: url("{{ asset('assets/fonts/cairo/Cairo-SemiBold.ttf') }}") format("truetype");
+        font-weight: 600;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: "Cairo";
+        src: url("{{ asset('assets/fonts/cairo/Cairo-Bold.ttf') }}") format("truetype");
+        font-weight: 700;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: "Cairo";
+        src: url("{{ asset('assets/fonts/cairo/Cairo-ExtraBold.ttf') }}") format("truetype");
+        font-weight: 800;
+        font-style: normal;
+        font-display: swap;
+    }
+
+    @font-face {
+        font-family: "Cairo";
+        src: url("{{ asset('assets/fonts/cairo/Cairo-Black.ttf') }}") format("truetype");
+        font-weight: 900;
+        font-style: normal;
+        font-display: swap;
+    }
     @page {
         size: A4 {{ $orientation }};
         margin: {{ $orientation === 'landscape' ? '10mm' : '12mm' }};
--- resources/views/prints/financial-summary-report-print.blade.php
+++ resources/views/prints/financial-summary-report-print.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>الملخص المالي</title>
 
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"> -->
 
     <x-erp.print-page-styles orientation="landscape" />
 </head>
