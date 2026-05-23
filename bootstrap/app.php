e18d7e45b7f21103f4340bea2b033e327da9571d
mo7ashraf <mohammed.ashraf2011@gmail.com>
2026-05-23 10:36:06 +0300

    Redirect employees from forbidden admin panel to employee panel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +18,19 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+            $isForbidden = $e instanceof AuthorizationException
+                || ($e instanceof HttpExceptionInterface && $e->getStatusCode() === 403);
+
+            if (
+                $isForbidden
+                && $request->is('admin', 'admin/*')
+                && auth()->check()
+                && auth()->user()?->hasRole('employee')
+            ) {
+                return redirect('/employee');
+            }
+
+            return null;
+        });
     })->create();
